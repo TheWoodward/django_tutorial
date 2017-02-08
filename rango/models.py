d4202d0cd55cf9ce710d1f243a78338c2e27c6b8
Edward Wood <2194801w@student.gla.ac.uk>
2017-02-08 17:58:57 +0000

    Chapter 8 Exercises

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,6 @@ from django.template.defaultfilters import slugify
 
 from django.db import models
 
-
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
--- rango/tests.py
+++ rango/tests.py
@@ -23,16 +23,6 @@ def test_slug_line_creation(self):
     cat.save()
     self.assertEqual(cat.slug, 'random-category-string')
 
-class IndexViewTests(TestCase):
-    def test_index_view_with_no_categories(self):
-        """
-        If no questions exist, an appropriate message should be displayed.
-        """
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "There are no categories present.")
-        self.assertQuerysetEqual(response.context['categories'], [])
-
 def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
@@ -40,16 +30,14 @@ def add_cat(name, views, likes):
     c.save()
     return c
 
-def test_index_view_with_categories(self):
-    """
-    Check to make sure that the index has categories displayed
-    """
-    add_cat('test',1,1)
-    add_cat('temp',1,1)
-    add_cat('tmp',1,1)
-    add_cat('tmp test temp',1,1)
-    response = self.client.get(reverse('index'))
-    self.assertEqual(response.status_code, 200)
-    self.assertContains(response, "tmp test temp")
-    num_cats = len(response.context['categories'])
-    self.assertEqual(num_cats, 4)
+class IndexViewTests(TestCase):
+    def test_index_view_with_no_categories(self):
+        add_cat('test', 1, 1)
+        add_cat('temp', 1, 1)
+        add_cat('tmp', 1, 1)
+        add_cat('tmp test temp', 1, 1)
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "tmp test temp")
+        num_cats = len(response.context['categories'])
+        self.assertEqual(num_cats, 4)
